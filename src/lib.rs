971dcb8b4222a6dd3e7f0be95dd393dcfd12c406
Nene Odonkor <38510646+neneodonkor@users.noreply.github.com>
2026-04-19 16:26:31 +0000

    Updated lib file

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,6 @@
-pub mod error;
-pub mod env_tomap;
 pub mod env;
+pub mod env_tomap;
+pub mod error;
 
 //pub use env::{parse, parse_with_options, Options};
 pub use error::*;
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
